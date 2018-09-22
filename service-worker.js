b1a26f27a7f46103d4bce832aedb6554a23cde85
Meo Huynh <mayoh727@gmail.com>
2018-09-22 16:58:08 -0700

    Create structure of an evt listener for installation

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,9 @@
 // Checks to see if the sw is registered
 console.log('Service Worker: Registered');
+
+// After the sw is registered, an installation event is fired
+// The code below listens for that event
+// The 'self' refers to the sw
+self.addEventListener('install', function(evt) {
+
+});
