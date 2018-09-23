e8696e95d38ed4a0db1a20e5fb7ea7f5fb0c8648
Meo Huynh <mayoh727@gmail.com>
2018-09-22 17:53:02 -0700

    Add open method w/ a promise to add all cacheFiles
    Used waitUntil method to wait for completion of installation.
    Then open method uses a string variable (cacheList)
    to add all elements (cacheFiles) to it as a list.
    You can find it through Dev Tools under Application > Cache Storage.
    
    Additional note: For awhile, nothing showed up and I wasn't sure why.
    Kept looking for any spelling errors, missing quotes, etc. NOTHING.
    
    Finally, I tried rearranging the cacheFiles array with the html files
    first, then css files, js files, etc., with images last.
    
    That seemed to have fixed it, but I wonder if that really was the issue?
    The order within the array? Hmm..
    
    Upon further research of why my cache items weren't showing up,
    I also found out I should include external links, so I added the
    leaflet js and css links as well.

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,44 @@
 // Checks to see if the sw is registered
 console.log('Service Worker: Registered');
 
+// Will contain cacheFiles elements
+// Will be used in conjuction with the installation event listener below
+// The string 'List' will appear in the Cache Storage
+const cacheList = 'List';
+
 // List of file names to cache
 // Comprises all file paths into strings
 // This is everything the app uses/requests to create what users see
 const cacheFiles = [
-  '/index.html',
-  '/restaurant.html',
-  '/css/styles.css',
-  '/data/restaurant.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/restaurant_info.js'
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
 ];
 
 // After the sw is registered, an installation event is fired
 // The code below listens for that event
 // The 'self' refers to the sw
 self.addEventListener('install', function(evt) {
-
+  // waitUntil method is used to wait until the installation evt is complete
+    evt.waitUntil(
+        caches.open(cacheList).then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
